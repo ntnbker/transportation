335981502e12a4b7099a484f62ead4d7c8b80dea
Jeff <ntnbker@gmail.com>
2018-10-16 11:03:02 +0700

    add reactstrap

--- src/routes/Counter/components/Counter.js
+++ src/routes/Counter/components/Counter.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Button } from 'reactstrap';
 
 export const Counter = ({ counter, increment, doubleAsync }) => (
   <div style={{ margin: '0 auto' }} >
     <h2>Counter: {counter}</h2>
-    <button className='btn btn-primary' onClick={increment}>
+    <Button className='btn btn-primary' onClick={increment}>
       Increment
-    </button>
+    </Button>
     {' '}
-    <button className='btn btn-secondary' onClick={doubleAsync}>
+    <Button className='btn btn-secondary mx-4' onClick={doubleAsync}>
       Double (Async)
-    </button>
+    </Button>
   </div>
 )
 Counter.propTypes = {
